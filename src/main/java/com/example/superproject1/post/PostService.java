434b328195fc8c3c56ac276947fd15141d821004
lhsfn3 <lhsfn3@gmail.com>
2024-06-14 22:46:48 +0900

    📰 : PostService add

---
+++ src/main/java/com/example/superproject1/post/PostService.java
@@ -0,0 +1,9 @@
+package com.example.superproject1.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+}
