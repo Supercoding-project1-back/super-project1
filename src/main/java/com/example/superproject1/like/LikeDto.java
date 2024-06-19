cc951644c6d76e105385161f0dbe3597caa6ba83
lhsfn3 <lhsfn3@gmail.com>
2024-06-19 22:47:19 +0900

    📰 : PostService add post create post search post update post delete

--- src/main/java/com/example/superproject1/like/LikeDto.java
+++ src/main/java/com/example/superproject1/like/LikeDto.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 public class LikeDto {
     @Getter
     @Setter
     @AllArgsConstructor
-    public class Post {
+    public static class Post {
         private int user_id;
         private int post_id;
     }
@@ -17,14 +18,14 @@ public class LikeDto {
     @Getter
     @Setter
     @AllArgsConstructor
-    public class MessageResponse {
+    public static class MessageResponse {
         private int message;
     }
 
     @Getter
     @Setter
     @AllArgsConstructor
-    public class LikeResponse {
+    public static class LikeResponse {
         private int user_id;
         private int post_id;
     }
